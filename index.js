b0630b09cafb3350ff21102a98cda42e1123f0c2
Annika Kriskovich <annika.krisko2013@gmail.com>
2022-12-22 14:44:35 -0600

    commit tree 3cf55329744e72ed75c50e98d99f353cf37f98aa

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    var bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob';
+}
